dc0da8b5c849ded66b0b00f04675567155ecb0a6
Satwik <satwik929@gmail.com>
2021-05-09 22:14:53 +0530

    Add files via upload
    Added scale_factor
    Changed interpolation in restore scaling to cv2.INTER_NEAREST_EXACT

--- hpacellseg/cellsegmentator.py
+++ hpacellseg/cellsegmentator.py
@@ -8,7 +8,7 @@ import numpy as np
 import torch
 import torch.nn
 import torch.nn.functional as F
-from skimage import util
+from skimage import transform, util
 
 from hpacellseg.constants import (MULTI_CHANNEL_CELL_MODEL_URL,
                                   NUCLEI_MODEL_URL, TWO_CHANNEL_CELL_MODEL_URL)
@@ -24,43 +24,14 @@ class CellSegmentator(object):
             self,
             nuclei_model="./nuclei_model.pth",
             cell_model="./cell_model.pth",
-            model_width_height=512,
+            model_width_height=None,
             device="cuda",
             multi_channel_model=True,
-            return_without_scale_restore=False
+            return_without_scale_restore=False,
+            scale_factor=0.25,
+            padding=False
     ):
-        """Class for segmenting nuclei and whole cells from confocal microscopy images.
 
-        It takes lists of images and returns the raw output from the
-        specified segmentation model. Models can be automatically
-        downloaded if they are not already available on the system.
-
-        When working with images from the Huan Protein Cell atlas, the
-        outputs from this class' methods are well combined with the
-        label functions in the utils module.
-
-        Note that for cell segmentation, there are two possible models
-        available. One that works with 2 channeled images and one that
-        takes 3 channels.
-
-        Keyword arguments:
-        nuclei_model -- A loaded torch nuclei segmentation model or the
-                        path to a file which contains such a model.
-                        If the argument is a path that points to a non-existant file,
-                        a pretrained nuclei_model is going to get downloaded to the
-                        specified path (default: './nuclei_model.pth').
-        cell_model -- A loaded torch cell segmentation model or the
-                      path to a file which contains such a model.
-                      The cell_model argument can be None if only nuclei
-                      are to be segmented (default: './cell_model.pth').
-        model_width_height --
-        device -- The device on which to run the models.
-                  This should either be 'cpu' or 'cuda' or pointed cuda
-                  device like 'cuda:0' (default: 'cuda').
-        multi_channel_model -- Control whether to use the 3-channel cell model or not.
-                               If True, use the 3-channel model, otherwise use the
-                               2-channel version (default: True).
-        """
         if device != "cuda" and device != "cpu" and "cuda" not in device:
             raise ValueError(f"{device} is not a valid device (cuda/cpu)")
         if device != "cpu":
@@ -100,27 +71,11 @@ class CellSegmentator(object):
         self.cell_model = cell_model.to(self.device)
         self.model_width_height = model_width_height
         self.return_without_scale_restore = return_without_scale_restore
+        self.scale_factor = scale_factor
+        self.padding = padding
 
     def _image_conversion(self, images):
-        """Convert/Format images to RGB image arrays list for cell predictions.
 
-        Intended for internal use only.
-
-        Keyword arguments:
-        images -- list of lists of image paths/arrays. It should following the
-                 pattern if with er channel input,
-                 [
-                     [microtubule_path0/image_array0, microtubule_path1/image_array1, ...],
-                     [er_path0/image_array0, er_path1/image_array1, ...],
-                     [nuclei_path0/image_array0, nuclei_path1/image_array1, ...]
-                 ]
-                 or if without er input,
-                 [
-                     [microtubule_path0/image_array0, microtubule_path1/image_array1, ...],
-                     None,
-                     [nuclei_path0/image_array0, nuclei_path1/image_array1, ...]
-                 ]
-        """
         microtubule_imgs, er_imgs, nuclei_imgs = images
         if self.multi_channel_model:
             if not isinstance(er_imgs, list):
@@ -173,34 +128,44 @@ class CellSegmentator(object):
 
         return cell_imgs
 
+    def _pad(self, image):
+          rows, cols = image.shape[:2]
+          self.scaled_shape = rows, cols
+          img_pad= cv2.copyMakeBorder(
+                    image,
+                    32,
+                    (32 - rows % 32),
+                    32,
+                    (32 - cols % 32),
+                    cv2.BORDER_REFLECT,
+                )
+          return img_pad
+
     def pred_nuclei(self, images):
-        """Predict the nuclei segmentation.
-
-        Keyword arguments:
-        images -- A list of image arrays or a list of paths to images.
-                  If as a list of image arrays, the images could be 2d images
-                  of nuclei data array only, or must have the nuclei data in
-                  the blue channel; If as a list of file paths, the images
-                  could be RGB image files or gray scale nuclei image file
-                  paths.
-
-        Returns:
-        predictions -- A list of predictions of nuclei segmentation for each nuclei image.
-        """
+        
 
         def _preprocess(images):
             if isinstance(images[0], str):
                 raise NotImplementedError('Currently the model requires images as numpy arrays, not paths.')
                 # images = [imageio.imread(image_path) for image_path in images]
             self.target_shapes = [image.shape for image in images]
-            images = [cv2.resize(image, (self.model_width_height, self.model_width_height))
-                      if image.shape[0] != self.model_width_height or image.shape[
-                1] != self.model_width_height else image
-                      for image in images]
+            #print(images.shape)
+            #resize like in original implementation with https://scikit-image.org/docs/dev/api/skimage.transform.html#skimage.transform.resize
+            if self.model_width_height:
+                images = np.array([transform.resize(image, (self.model_width_height,self.model_width_height)) 
+                                  for image in images])
+            else:
+                images = [transform.rescale(image, self.scale_factor) for image in images]
+
+            if self.padding:
+              images = [self._pad(image) for image in images]
 
             nuc_images = np.array([np.dstack((image[..., 2], image[..., 2], image[..., 2])) if len(image.shape) >= 3
                                    else np.dstack((image, image, image)) for image in images])
+            
             nuc_images = nuc_images.transpose([0, 3, 1, 2])
+            #print("nuc", nuc_images.shape)
+
             return nuc_images
 
         def _segment_helper(imgs):
@@ -218,65 +183,50 @@ class CellSegmentator(object):
         preprocessed_imgs = _preprocess(images)
         predictions = _segment_helper(preprocessed_imgs)
         predictions = predictions.to("cpu").numpy()
+        #dont restore scaling, just save and scale later ...
         predictions = [self._restore_scaling(util.img_as_ubyte(pred), target_shape)
                        for pred, target_shape in zip(predictions, self.target_shapes)]
         return predictions
 
     def _restore_scaling(self, n_prediction, target_shape):
         """Restore an image from scaling and padding.
-
         This method is intended for internal use.
         It takes the output from the nuclei model as input.
         """
         n_prediction = n_prediction.transpose([1, 2, 0])
+        if self.padding:
+          n_prediction = n_prediction[
+                32 : 32 + self.scaled_shape[0], 32 : 32 + self.scaled_shape[1], ...
+            ]
         n_prediction[..., 0] = 0
         if not self.return_without_scale_restore:
             n_prediction = cv2.resize(
                 n_prediction,
                 (target_shape[0], target_shape[1]),
-                interpolation=cv2.INTER_NEAREST,
+                #try INTER_NEAREST_EXACT
+                interpolation=cv2.INTER_NEAREST_EXACT,
             )
         return n_prediction
 
     def pred_cells(self, images, precombined=False):
-        """Predict the cell segmentation for a list of images.
-
-        Keyword arguments:
-        images -- list of lists of image paths/arrays. It should following the
-                  pattern if with er channel input,
-                  [
-                      [microtubule_path0/image_array0, microtubule_path1/image_array1, ...],
-                      [er_path0/image_array0, er_path1/image_array1, ...],
-                      [nuclei_path0/image_array0, nuclei_path1/image_array1, ...]
-                  ]
-                  or if without er input,
-                  [
-                      [microtubule_path0/image_array0, microtubule_path1/image_array1, ...],
-                      None,
-                      [nuclei_path0/image_array0, nuclei_path1/image_array1, ...]
-                  ]
-
-                  The ER channel is required when multichannel is True
-                  and required to be None when multichannel is False.
-
-                  The images needs to be of the same size.
-        precombined -- If precombined is True, the list of images is instead supposed to be
-                       a list of RGB numpy arrays (default: False).
-
-        Returns:
-        predictions -- a list of predictions of cell segmentations.
-        """
 
         def _preprocess(images):
             self.target_shapes = [image.shape for image in images]
             for image in images:
                 if not len(image.shape) == 3:
                     raise ValueError("image should has 3 channels")
-            images = np.array([cv2.resize(image, (self.model_width_height, self.model_width_height))
-                               if image.shape[0] != self.model_width_height or image.shape[1] != self.model_width_height
-                               else image
-                               for image in images])
+            #resize like in original implementation with https://scikit-image.org/docs/dev/api/skimage.transform.html#skimage.transform.resize
+            if self.model_width_height:
+                images = np.array([transform.resize(image, (self.model_width_height,self.model_width_height)) 
+                                  for image in images])
+            else:
+                images = np.array([transform.rescale(image, self.scale_factor, multichannel=True) for image in images])
+
+            if self.padding:
+              images = np.array([self._pad(image) for image in images])
+
             cell_images = images.transpose([0, 3, 1, 2])
+
             return cell_images
 
         def _segment_helper(imgs):
